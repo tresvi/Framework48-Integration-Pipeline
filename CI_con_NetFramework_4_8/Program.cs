f455bd642a3dd9cb5931d654f79974e38fe071b8
tresvi <69699106+tresvi@users.noreply.github.com>
2023-07-31 01:22:12 -0300

    Subida Inicial

---
+++ CI_con_NetFramework_4_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_con_NetFramework_4_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esto es un proyecto de consola de .NET Framework");
+        }
+    }
+}
