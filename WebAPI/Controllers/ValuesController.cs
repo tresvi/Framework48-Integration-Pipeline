d282be58c7d664a04b56d7448a1670721cf91b0d
tresvi <69699106+tresvi@users.noreply.github.com>
2023-08-02 01:19:25 -0300

    Agregado de API Web MVC

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Instrumentation;
+using System.Web.UI;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    //Para acceder a SWAGGER: http://localhost:64447/swagger/ui/index
+    public class ValuesController : ApiController
+    {
+        private List<Empleado> _empleados = new List<Empleado>()
+        { 
+            new Empleado(1, "Homero", "Simpson"),
+            new Empleado(2, "Charles M.", "Burns"),
+            new Empleado(3, "Seymour", "Skinner")
+        };
+
+        // GET api/values
+        [ResponseType(typeof(List<Empleado>))]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(_empleados);
+        }
+
+        // GET api/values/5
+        [ResponseType(typeof(Empleado))]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var empleado = _empleados.Find(x => x.Id == id);
+            if (empleado == null)
+                return NotFound();
+            else
+                return Ok(empleado);
+        }
+
+        // POST api/values
+        [ResponseType(typeof(Empleado))]
+        public async Task<IHttpActionResult> Post([FromBody] Empleado empleado)
+        {
+            _empleados.Add(empleado);
+            return CreatedAtRoute("", new { id = empleado.Id}, empleado );
+        }
+
+    }
+}
---
+++ WebAPI/Models/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Empleado(int id, string nombre, string apellido) 
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+    }
+}
